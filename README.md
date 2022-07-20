13ef31957fa54ef21089e69517c5e4c5772022a0
Semm6 <sem.dewilde6@gmail.com>
2022-07-20 14:22:43 +0200

    Added entity

--- README.md
+++
@@ -1,1 +0,0 @@
-# bulkhouse-backend
---
+++ bulkhouse-backend/src/test/java/com/example/bulkhousebackend/BulkhouseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bulkhousebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkhouseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
